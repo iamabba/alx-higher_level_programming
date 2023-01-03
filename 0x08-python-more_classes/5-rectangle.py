8b4538b53ad55c6a07cbd65f004a88bf32166fc6
iamabba <iamabbamusa@gmail.com>
2023-01-03 05:16:59 -0800

    Detect instance deletion

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -51,7 +51,6 @@ class Rectangle:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
-
     def __str__(self):
         """Return the printable representation of the Rectangle.
         Represents the rectangle with the # character.
