c8e8adfde414eb13bfac9d8fae5e4ce263be0dde
iamabba <iamabbamusa@gmail.com>
2023-03-14 17:20:15 +0000

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+
+const { list } = require('./100-data');
+const factorIndex = (value, index) => value * index;
+const newList = list.map(factorIndex);
+console.log(list);
+console.log(newList);
